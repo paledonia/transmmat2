495df95682ac318b1649d92f63b63b3c3e0d2ac7
acastellanosramirez <acastellanosramirez@gmail.com>
2024-12-10 16:07:18 +0000

    Final.final

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ prueba = st.Page("paginas/dos.py", title = "Prueba")
 vectores = st.Page ("paginas/vectores.py", title = "Vectores") 
 deter = st.Page("paginas/deter.py", title = "Determinantes") 
 trans = st.Page("paginas/trans.py", title = "Transformaciones Lineales") 
-yo = st.Page("paginas/yo.py", title = "Yo") 
+yo = st.Page("paginas/yo.py", title = "Andres Castellanos") 
 quiz = st.Page("paginas/quiz.py", title = "Quiz") 
 mul_trans = st.Page("paginas/mul_trans.py", title = "Multiplicacion vista como Tranformaciones lineales")  
 sub_es = st.Page("paginas/sub_es.py", title = "Subespacios Vectoriales") 
@@ -15,7 +15,7 @@ sub_es = st.Page("paginas/sub_es.py", title = "Subespacios Vectoriales")
 
 
 
-pg = st.navigation({"Corazon del Algebra lineal": [intro,vectores,sub_es,trans ], "Prueba tus conocimientos":[quiz], "Quien soy":[yo], "prueba":[ prueba]}) 
+pg = st.navigation({"Corazon del Algebra lineal": [intro,vectores,sub_es,trans ], "Prueba tus conocimientos":[quiz], "Quien soy":[yo]}) 
 
 pg.run()
 
---
+++ paginas/yo.py
@@ -0,0 +1,27 @@
+import streamlit as st 
+import numpy as np
+import matplotlib.pyplot as plt 
+from func_vec import * 
+
+st.title("Quien soy") 
+st.divider() 
+st.subheader("*Andres Castellanos*") 
+
+st.markdown("""
+Mi nombre completo es **Andres Mauricio Castellanos Ramirez**. Mi ciudad de prodecedencia es Barrancabermeja. Alli genere mis primeros 
+lazos tanto con la musica, el futbol y las matematicas. Estas tres disciplinas han marcado mi vida de manera sustancial. Ya que no me veo
+dejando de hacer ninguna de estas tres actividades. Son muy enriquecedoras tanto para el espiruto, cuerpo y mente. 
+""") 
+
+st.image("https://scontent.fbaq9-1.fna.fbcdn.net/v/t39.30808-6/440120050_7837841132893775_2934785061605323091_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEdqIRhhci3TklcnSlOqreeuHfFCg3XARe4d8UKDdcBF2FH_GeO8qhNmDKwtaQ0fffeySqQZBjka367T3EIwoyk&_nc_ohc=-IE_5-EWZq8Q7kNvgFELe59&_nc_zt=23&_nc_ht=scontent.fbaq9-1.fna&_nc_gid=App9ESLiDd5D6NNOLJgSMwh&oh=00_AYAiLdKrVUGF0j6zf_fk7VZQNhx22aHZXf6bgfRcOVfgqQ&oe=675E2BD5") 
+
+st.markdown ("""
+Me caracterizo por ser alegre, sobre todo cuanto canto. Me gusta transmitir alegría a las personas que tengo a mi al rededor. Aveces soy un poco terco, 
+pero creo que algunos aspectos de mi vida ha funcionado. 
+
+Mi estancia en la UIS (Universidad Industrial de Santander) ha sido de idas y vueltas, pero quiero sacar mi carrera adelante. Ya que 
+quiero descubrir lo que tienen las matematicas para mostrarme.  
+
+""") 
+
+st.image ("https://scontent.fbaq9-1.fna.fbcdn.net/v/t39.30808-6/442437831_7948058148538739_3449561923385410723_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEvfNcIXxwTj67oBr-G66loWcNXbCIaajxZw1dsIhpqPFoOIAgkTzj6W3bR03mhAcUvQZ6jwbhfU6AzQxEaza_F&_nc_ohc=qFhfGR-8cxcQ7kNvgEoLG0M&_nc_zt=23&_nc_ht=scontent.fbaq9-1.fna&_nc_gid=A8CBjku-Z6q9SYE3laCxZj4&oh=00_AYB_AqnOsNA5ERcfLqHg_iE1mQ-IFBjo35OCIZSXirCEEA&oe=675E3477")
